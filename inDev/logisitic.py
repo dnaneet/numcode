838c52f700b9afd1caf8cfb816c080654add935f
Aneet D Narendranath <dnaneet@mtu.edu>
2017-02-11 09:05:32 -0500

    Add files via upload

---
+++ inDev/logisitic.py
@@ -0,0 +1,28 @@
+import numpy as np
+from scipy.integrate import ode
+import matplotlib.pyplot as plt
+
+
+def logistic(t, y, r):
+    return r * y * (1.0 - y)
+
+r = .05
+t0 = 0
+y0 = 1e-5
+t1 = 15000.0
+
+backend = 'dopri5'
+# backend = 'dop853'
+solver = ode(logistic).set_integrator(backend)
+
+sol = []
+def solout(t, y):
+    sol.append([t, y])
+solver.set_solout(solout)
+solver.set_initial_value(y0, t0).set_f_params(r)
+solver.integrate(t1)
+
+sol = np.array(sol)
+
+plt.plot(sol[:,0], sol[:,1], 'b.-')
+plt.show()
