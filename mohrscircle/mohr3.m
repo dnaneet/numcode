41825c26a332eb083ca5c8bb300231bc68069e99
Aneet D Narendranath <dnaneet@mtu.edu>
2018-04-04 15:20:23 -0400

    Add files via upload

--- mohrscircle/mohr3.m
+++ mohrscircle/mohr3.m
@@ -2,39 +2,43 @@
 clear;
 close all;
 clc;
+%Original state of stress, (sxx, txy, syy)
 sxx = 1000.;
 syy = 0;
 txy = 0;
 x = [sxx; -txy];
 y = [syy; txy];
-t = -10; %degree. Negative=clockws, positive=counterc/w
-         %MATLAB needs angles in radian.  
+t = -10; %Angle through which original state of stress is rotated
+         %in degree. Negative=clockws, positive=counterc/w
+         % MATLAB needs angles in radian.  This can be circumvented by
+         % using sind() or cosd()....
 twoTheta=deg2rad(2*t);
 twoTheta = 2*t; %degree
 th = 0:pi/50:2*pi;
 r=sqrt((0.5*(sxx-syy))^2 + txy^2);
-
-x_cen=0.5*(sxx + syy);
-y_cen=0.;
-%% Mohrs circle with original state of stress
-r=sqrt((0.5*(sxx-syy))^2 + txy^2); %Radius of Mohr's circle
+%Average value of stress OR center of circle
 x_cen=0.5*(sxx + syy);
 y_cen=0.;
 s_ave = x_cen;
-s_1 = s_ave+r;
-s_2 = s_ave-r;
+%% Mohrs circle with original state of stress
+r=sqrt((0.5*(sxx-syy))^2 + txy^2); %Radius of Mohr's circle
+s_1 = s_ave+r; %principal stress
+s_2 = s_ave-r; %principal stress
+%Creating the circle
 xunit = r * cos(th) + x_cen;
 yunit = r * sin(th) + y_cen;
 hold on;
 plot(xunit, yunit, 'k', 'Linewidth', 1);
 scatter(x_cen, y_cen, 'bx' , 'Linewidth', 1);
+
+%Plotting the original plane of stress on the circle
 scatter(x(1), x(2), 'rx', 'Linewidth', 1)
 scatter(y(1), y(2), 'rx', 'Linewidth', 1)
 line([x(1), y(1)],[x(2), y(2)],'LineWidth',0.2,...
    'Color',[1,0,0],'LineStyle','-')
 
 line([-1.25*max(s_1,s_2), 1.25*max(s_1,s_2)],[0,0],'LineStyle','-','LineWidth',0.2)
-%% 
+%% Rotation of original state of stress through theta (twoTheta in "Mohr's circle world").
 x_prime_x = (x(1)-x_cen)*cosd(twoTheta) - (x(2) - y_cen)*sind(twoTheta) + x_cen;
 x_prime_y = (x(1)-x_cen)*sind(twoTheta) + (x(2) - y_cen)*cosd(twoTheta) + y_cen;
 
@@ -46,4 +50,4 @@ scatter(y_prime_x, y_prime_y, 'k*')
 
 line([x_prime_x, y_prime_x],[x_prime_y, y_prime_y],'LineWidth',0.2,...
    'Color',[1,0,1],'LineStyle','-')
-
+%eof
